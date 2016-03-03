0f481096b18e9dd25fbe623764b3bc8aead3d22c
gowaa13 <war_game@livemail.tw>
2016-03-03 16:22:53 +0800

    first version in rwd blog

---
+++ js/lang.js
@@ -0,0 +1,188 @@
+var resources = {
+    "en": {
+        "translation": {
+            "title": "My Responsive Blog: Picture Story",
+            "h1": "My responsive blog",
+            "storys": "My Story",
+            "href": [{
+                    "title": "go to my portfolio website :D",
+                    "source": "HomePage for udacity course examples"
+                },
+                { "social": ["twitter", "fb", "digg", "google+"] }, {
+                    "title": "View source for this page on GitHub",
+                    "text": "Source on GitHub"
+                }
+            ],
+            "story": [{
+                "intro": "This is a Responsive Website Blog that I learned from udacity course \u263A!Even this demo is very simple but I still have learned many stuff, and the following are the photo that I taked about the story I'll say some stories. ",
+                "sample": 'the svg logo images under the text is a sample from the udacity course "Responsive Images", if You want learn more, you can click the logo sample and see  the course source code or go to the udacity website '
+            }, {
+                "figcaption": "The end in XinwuGreenTunnel, beach in Yongan Fishing Port ",
+                "Begin": "This place is my family travel at here  in Summer on 2013, at that time I played the bicycle with my nephews and see a lot of beautiful scenery, of course we also ate many delicious Hakka food, like Hakka Vegetable buns、Red buns、Hakka Mochi and so on.",
+                "Second": "After that we were so tired that we decided to visit Yongan Fishing Port to take a break, formerly known as Kantouwu Port, Yongan Fishing Harbor is located at the estuary of Shezu Creek. A great recreational place for families to cycle together and feast on seafood, it is not far from the Green Tunnel and is easily accessible by bicycle.",
+                "Then": "Within Yongan Fishing Harbor, there is a tourist fish market. The building is in the shape of a lobster with two transparent spiral staircases. In addition, Yongan Seaview Bridge is a huge bridge across the Yongan Fishing Harbor. Every evening at 5 p.m. the lights will brighten the whole bridge, whose reflection on the sea is a romantic.",
+                "Fin": "Finally, we have a luscious seafood dinner at a restaurant in the tourist fish market on this day, maybe thus experiencing is one of the happiest day of my this life !"
+            }, {
+                "figcaption": "Civil Conscious Awaken On That Day",
+                "Begin": "One death of an army conscript continues to send political shockwaves through Taiwan, with anonymous online organizers rallying more than 110,000 protesters to flood the streets of Taipei in 2013.",
+                "Second": "Many young people were among the protesters clad in white tops who marched on the Presidential Office on August 3.While protests are common in Taiwan, the suspicious death of 24-year-old army conscript Hung Chung-chiu has struck a particularly raw nerve with ordinary citizens.",
+                "Then": "Many people found the government's investigation of Mr. Hung's death unsatisfactory, especially the vague initial answers given by officials. Meanwhile, the event has stirred up wider grievances against the island's conscription military and President Ma Ying-jeou's administration.",
+                "Fin": "This event changes Taiwan's future, it is the result of good or bad that maybe only God knows."
+
+            }, {
+                "figcaption": "A dazzling nightscape in My hometown",
+                "Begin": "I had forgotten what day is the day, but the color of this sky is much more picturesque, take it down",
+                "Second": "Sometimes I think one thing what is the meaning of life? Up to today it is still one unsolved mysteries of the World. So don't just keep going on thinking, appreciate what you have right now. seize the moment.",
+                "Then": "Life without regret, dead things like a gust of wind; life without regret, it will still have to come; life need not be annoyed, it's no use crying over spilt milk; life need not be lamented, loss of self-confidence will lose everything!",
+                "Fin": "The life stage for anyone is not everything.The road of life is a spiral path, only swagger, flexible turning, could rise to our aims. Cuz we have only one life and one chance to do all the things we want to do, jsut go where we wanna to go；be what we wanna to be !",
+               
+
+            }, {
+                "figcaption": "About Northern Mioali ArtCenter",
+                "Begin": "On the basis of balance development of whole area in Miaoli County, and to create a cultural space which fuses with beauty, art and recreation, the founding of Northern Miaoli Art Center is greatly advanced by Miaoli Government. Initially, it aims at the integration of art resource, promoting local humanities and artistic temperament. Regional cultural characteristic to be recognized by national wide is another emphasis as well. The bringing-in of culture across straits and international , will make Miaoli County a place full of enjoyment for culture life.",
+                "Second":  "“Northern Miaoli Art Center” is a modern building with 5 floors above and 2 floors underground, the square measure of whole building is 92.55 acres , defined as a performing location up to “County and city level” accordingly. There are 1,090 seats for Performing Hall, 230 seats for Experimental Theater, Audiovisual Center, Outdoor Performing Stage, Exhibition Area and Artist Studios are mapped out. Suitable for all kinds of music, drama, dance performing, exhibitions, workshops and large-scale gatherings.",
+                "Fin": "The arts center is  full of art in this country as well, there are many good activities."
+            }, {
+                "figcaption": "Attack Rubber Duck in Taoyuan",
+                "Begin": "On that day, mankind received a grim reminder. We lived in fear of the Rubber Duck and were disgraced to live in these cages we called walls.(Big mistake)",
+                "Second": "Rubber Duck is any of several giant floating sculptures designed by Dutch artist Florentijn Hofman. These were built in various sizes, ranging from the prototype which measured 1 inch to the one created in 2007 that is the largest rubber duck in the world, measuring 26×20×32 metres (85×66×105 ft) and weighing over 600 kg (1,300 lb).",
+                "Then": '2013 Taoyuan Land Art Festival  is a large exhibition  at  Xinwu Township, Taoyuan County  from  October 26, 2013 to  November 10, 2013. The art installations, named "Rubber Duck" at Duck exhibition in HouHu pond on display by the Dutch conceptual artist, Florentijn Hofman who is the author of a giant yellow duck artwork,  a series of pink polka dot inflatables at dotdot exhibition in Xinwu Pei is on display among Japanese contemporary artist, Yayoi Kusama\'s  creation named "footprint of life" 15 works,and also the giant pink Lotuses named "Lotus series" at lotus pond pavilion in OverHill 58  exhibit by Contemporary Artists, Choi Jung of Korea (Jeong-Hwa Choi)',
+                "Fin": "To awaken the awareness of sustaining ecological environments and share the glamorous of ponds with the citizens, the festival connected the works of domestic and international artists with the ponds."
+
+            }, {
+                "figcaption": "A nightscape photo on YangMei road is taken by myself",
+                "Begin": "Actually, I'm not just that into photography.But in the traning period in YangMei , I got a photography class so that I  studied hard about that.",
+                "Second": "This photo is used the skill called Long Exposure Photography, that involves using a long-duration shutter speed to sharply capture the stationary elements of images while blurring , smearing, or obscuring the moving elements. ",
+                "Then": "It captures one element that conventional photography does not: time. When a scene includes both stationary and moving subjects (for example, a fixed street and moving cars or a camera within a car showing a fixed dashboard and moving scenery), a slow shutter speed can cause interesting effects, such as light trails.",
+                "Fin": "Long Exposure Photography is also often used in a night-time setting, where the lack of light forces longer exposures, if maximum quality is to be retained.Increasing ISO sensitivity allows for shorter exposures, but substantially decreases image quality through reduced dynamic range and higher noise.By leaving the camera's shutter open for an extended period of time, more light is absorbed, creating an exposure that captures the entire dynamic range of the digital camera sensor or film. If the camera is stationary for the entire period of time that the shutter is open, a very vibrant and clear photograph can be produced.",
+                "PS":"P.S.: After crossed period of time, I am too lazy for photography to forget almost my photography skills. So sad!"
+
+            }, {
+                "figcaption": "A DepressedBear",
+                "Begin": "Because compulsory land acquisition, one bear was sitting alone on the ground, he felt disappointed, he didn't know how to do, he got nothing to do.",
+                "Second": "For Urban Development, the government is taking  a series of steps to  be urbanization and modernization, although it truly has brought us a lot of benefits but has created some serious problems as well. But if The event like  compulsory land acquisition  happens for me someday,  I have no idea to do something.  ",
+                "Then": "There is always give and take in our life, maybe all that we can do is only do our best to relish this remarkable ride. The journey is what brings us happiness maybe not the important destination.",
+                "Fin": "Until now, the depressedbear still has been sitting stolidly on the ground, staring at the blue sky! "
+
+            }],
+            "opening": {
+                "T1": "It's time to learning Some New",
+                "T2": "Learning HTML5",
+                "T3": "Learning CSS3",
+                "T4": "Learning Javascript",
+                "T5": "Learining Everything You Want",
+                "T6": "This Summer",
+                "T7": "Prepare Be",
+                "title": "Front-end developer"
+            }
+
+        }
+    },
+    "tw": {
+        "translation": {
+            "title": "我的響應式部落格:看圖說故事",
+            "h1": "我的響應式部落格",
+            "storys": "我的故事",
+            "href": [{
+                    "title": "到我的作品集 :D",
+                    "source": "udacity響應式圖片課程開源碼"
+                },
+                { "social": ["推特", "掘客", "臉書", "谷歌+"] }, {
+                    "title": "看此頁原始碼於GitHub",
+                    "text": "原始碼於版本控制網站"
+                }
+            ],
+            "option": {
+                "open": "開啟",
+                "close": "關閉"
+            },
+            "story": [{
+                "intro": "這是一個我參考udacity課程做的小小響應式部落格 \u263A!雖然畫面稍微簡潔但也學了不少東西，以下我個人去過的地方和拍的一些圖片，並稍微敘述一些故事。",
+                "sample": "下面那個是udacity課程的svg版的標誌範例，按下標誌可以到他們響應式圖片課程的全部範例檔。"
+
+            }, {
+                "figcaption": "新屋綠色隧道盡頭，永安漁港海邊",
+                "Begin": "這是2013年夏天與家人旅遊所到之處，那時與姪子女共同騎腳踏車，沿途看到許多美景，也吃了許多好吃的客家美食，像是客家菜包、紅粄、客家麻糬等等。",
+                "Second": "經過一陣子後，我們實在太累了，所以決定到永安漁港休息片刻，永安漁港位於社子溪出海口，原名崁頭屋港，是親子們一起騎車啖海鮮美食的好去處，離綠色隧道不遠的路程就可以輕鬆騎到，真的是全家人休閒娛樂的好去處。",
+                "Then": "永安漁港內建有一棟觀光漁市，外觀以龍蝦為設計風格，並有兩道透明螺旋梯。另外，永安觀海橋是永安漁港的一座跨港大橋，每逢傍晚五點後燈光點亮整條橋，照映在海面上非常浪漫。",
+                "Fin": "到了最後，我們在那觀光漁市下吃下一道道甜美海鮮大餐，或許，這經歷會是我這一生中最快樂的一天之一吧！"
+
+            }, {
+                "figcaption": "那天 洪仲丘案件導致台灣公民意識覺醒",
+                "Begin": "一名徵召入伍士兵的死亡在當地產生的政治影響持續發酵，匿名網上組織者召集了逾11萬名抗議者在臺北街頭示威遊行。抗議者們身穿白色上衣，在總統府前舉行遊行，他們當中有著許多年輕人。",
+                "Second": "儘管抗議活動在臺灣很常見，但這位名叫洪仲丘的24歲士兵的離奇死亡令臺灣普通民眾感到格外憤怒。",
+                "Then": "許多人認為政府對洪仲丘死亡案的調查無法令人滿意，特別是官方最初對此案的模棱兩可的解答。與此同時，此案引發了對義務兵役制和總統馬英九政府的廣泛抱怨。",
+                "Fin": "這事件改變了整個台灣未來，是好是壞大概只有上帝知道了。"
+
+            }, {
+                "figcaption": "某日 家鄉的夜景",
+                "Begin": "我已經忘了是哪一天，但天空的顏色很漂亮，就把它拍下來了。",
+                "Second": "有時候我在想人生的意義是什麼？直至今日，仍是未解之謎。所以，別再想了，把握當下吧。",
+                "Then": "人生不必後悔，逝去的事物就像一陣風就過去了；人生不必遺撼，該來的終究還是會來的；人生不必懊悔，灑出去的水也不會再收回來了；人生更不必哀嘆，失去自信就將失去一切！",
+                "Fin": "人生具有不定性，階段下的處境並不代表一切。人生道路猶如一條螺旋上升的路，只有昂首闊步，彈性轉個方向，才可能達到自身的理想。因為我們只有一次生命，一個機會去做任何那些我們想做的事，就去我們想去的所在，成為我們想成為的人吧！"        
+            }, {
+                "figcaption": "關於苗北藝文中心",
+                "Begin": "為使苗栗縣全境區域發展均衡，營創美藝與生活休閒融合的文化空間，由苗栗縣政府興建「苗北藝文中心」，初衷系以整合苗北生活圈的藝文資源，提升當地人文素養與藝術氣息為主，亦著重將當地特色文化推向國際舞台，並適度將兩岸與國藝際術文化引入，使苗栗縣成為享受文化生活的快樂園地。",
+                "Second": "「苗北藝文中心」為一座地上5層，地下2層的現代建築，整體建物面積合計有0.9255公頃，茲將苗北藝文中心層級定位為「縣市級」的表演場所，主要空間規劃為演藝廳(1090席)、實驗劇場(230席)、視聽中心、戶外展演區、藝文展覽區及藝術創作家駐站工作室，發展機能係為適合國內外各類型音樂、戲劇、舞蹈表演及展覽、研習訓練機能與大型集會使用。",
+                "Fin": "這個藝文中心多少也讓這個鄉村充滿藝術氣息，也有許多不錯的活動在此。"
+
+            }, {
+                "figcaption": "進擊的黃色小鴨在桃園",
+                "Begin": "那一天，人類終於回想起了，曾經一度被黃色小鴨所支配的恐怖，還有被囚禁於鳥籠中的那份屈辱。(大誤)",
+                "Second": "黃色小鴨是荷蘭概念藝術師弗洛倫泰因·霍夫曼所創作的巨型藝術品，先後製作了多個款式，其中在2007年所製作的黃色小鴨是世界上體積最龐大的作品，體積為26×20×32公尺(85×66×105英呎)，重達600公斤(1,300磅)。",
+                "Then": "2013桃園縣地景廣場藝術節是臺灣桃園縣政府於2013年10月26日至11月10日在桃園縣新屋鄉舉辦的大型展覽活動。小鴨展區後湖塘展出的作品為荷蘭概念藝術師弗洛倫泰因·霍夫曼所創作的巨型黃色小鴨藝術品，點點展區新屋陂則展出日本當代藝術家草間彌生（Kusama Yayoi）的創作「生命的足跡」15個作品，蓮花展區過嶺58號池展出韓國當代藝術家崔正化（Jeong-Hwa Choi）的「蓮花系列」。",
+                "Fin": "為了喚醒維持生態環境的意識，分享池塘的美景給市民，這個藝術節將國內外藝術家的作品與池塘結合。"
+
+            }, {
+                "figcaption": "在楊梅職訓學會的夜拍",
+                "Begin": "事實上我並沒有那麼喜歡攝影，但在楊梅職訓那段期間，有一個攝影課，既然有這種課程那就好好努力研究學習看看吧！",
+                "Second": "這張照片使用了一種攝影技巧，被稱之為長時間曝光。它牽涉到你使用一個長時間快門速度來大幅捕捉圖片的靜止元素，與此同時它也模糊、塗抹、或含糊掉其影像中移動的元件。",
+                "Then": "它抓住了傳統攝影沒有一個要素：時間。當一個場景包括固定和移動的物體時（例如，一個固定的街道和移動的汽車或汽車內的攝像頭呈現出固定的儀表盤和移動的風景），較慢的快門速度就會產生有趣的效果，如光跡。",
+                "Fin": "長時間曝光攝影通常用於夜間攝影，因為沒有了光害可以有更長時間的曝光，但如果想要高品質的照片可能就要保留使用這種技巧。提高ISO感光度可以允許更短的曝光，但將大大降低影像品質因為減少了動態範圍和產生高感噪點。若將照相機的快門打開較長的時間後，會有更多的光被吸收進來，此時數位照相機的感測器或影片就能創造一個可以捕捉整個動態範圍的曝光。如果相機的快門穩定處於打開狀態時，那麼一個非常有活力和清晰的照片就會在此產生。",
+                 "PS":"P.S.: 經過一段時間後，由於我太懶散聚焦在攝影上導致我早就幾乎忘光所有的攝影技巧了...。(著實令人傷心)"
+
+            }, {
+                "figcaption": "迷失的小熊",
+                "Begin": "因為土地強制徵收，一隻小熊孤單的坐在地上，他感到非常失望，他不知道該怎麼辦，他無事可做。",
+                "Second": "為了都市發展，政府持續採取一連串的措施來達成都市化與現代化，雖然它著實帶給我們許多好處，卻也引起許多嚴重的問題。如果有一天像是強制土地徵收這種事件發生在我身上，我還真不知道我會怎麼做。",
+                "Then": "在我們的生命當中總是有失有得，我們能做的就只有盡其所能，珍惜這趟不凡的人生旅程，這個旅程若帶給你快樂幸福的話，那麼最後結果或許也沒那麼重要了。",
+                "Fin": "直到如今，那個迷失的小熊仍然呆坐在地上，凝視著那被憂愁色渲染後的天空。"
+
+            }],
+            "opening": {
+                "T1": "是時候準備學新東西了!",
+                "T2": "學習寫程式",
+                "T3": "學著看設計",
+                "T4": "學會做人處世之道",
+                "T5": "學習任何我們想要的",
+                "T6": "今年夏天",
+                "T7": "準備啟程",
+                "title": "前端工程師之路"
+            }
+
+        }
+    }
+};
+
+$(document).ready(function() {
+
+    i18n.init({
+        "lng": 'en',
+        "resStore": resources,
+        "fallbackLng": 'tw'
+    }, function(t) {
+        $(document).i18n();
+        /*openingEffect*/
+        $("#os-phrases > h2").lettering('words').children("span").lettering().children("span").lettering();
+    });
+
+    $('.lang').click(function() {
+        var lang = $(this).attr('data-lang');
+        i18n.init({
+            lng: lang
+        }, function(t) {
+            $(document).i18n();
+        });
+        $("#os-phrases > h2").lettering('words').children("span").lettering().children("span").lettering();
+    });
+});
