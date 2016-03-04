88fa6b6adf13f0146224b36c02888f5d5e76922e
gowaa13 <war_game@livemail.tw>
2016-03-04 11:31:22 +0800

    change sourcecode href  and add gotop tag

--- js/main.js
+++ js/main.js
@@ -13,4 +13,18 @@ WebFontConfig = {
 
 
 
-
+$(document).ready(function(){
+    // gotop
+    $(".gotop").click(function(){
+        $("html,body").animate({
+            scrollTop:0
+        },1200);
+    });
+    $(window).scroll(function() {
+        if ( $(this).scrollTop() > 300){
+            $('.gotop').fadeIn("fast");
+        } else {
+            $('.gotop').stop().fadeOut("normal");
+        }
+    });
+});
